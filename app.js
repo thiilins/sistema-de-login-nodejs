840d886118b7bbd8f79a1dc5691366fe6c63a3b4
Thiago Lins <taglins@gmail.com>
2021-09-10 20:26:43 -0300

    Estrutura de pastas  Criada Projeto iniciado

---
+++ app.js
@@ -0,0 +1,34 @@
+const port = 3000;
+const maintenance = false;
+
+const express = require("express");
+const path = require("path");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
+app.use(
+  session({
+    secret: "6F8FA2A42E3BA185DFC0FB",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static(path.resolve("src", "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.use((req, res, next) => {
+  res.status(404).render("pages/404");
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`O servidor está rodando em http://localhost:${port}`);
+});
